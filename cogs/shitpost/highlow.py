cd8705757f366bf7f23057faf4b34634e90f2200
Cubxfy <97477068+Cubxfy@users.noreply.github.com>
2025-02-01 13:14:35 -0500

    database works
    got database to work. High Score now displayed on game start

--- cogs/shitpost/highlow.py
+++ cogs/shitpost/highlow.py
@@ -13,6 +13,7 @@ class Buttons(discord.ui.View):
         self.random_number = random.randint(1, 100)
         self.count = 0      
         
+        #DB initialisation
         self.conn = sqlite3.connect("gambling.db")
         self.cursor = self.conn.cursor()
         
@@ -20,7 +21,7 @@ class Buttons(discord.ui.View):
             CREATE TABLE IF NOT EXISTS highlow (
                 highest_streak INTEGER,
                 guild_id INTEGER,
-                user_id INTEGER,
+                user_id INTEGER UNIQUE
             )
         ''')
         self.conn.commit()
@@ -29,41 +30,43 @@ class Buttons(discord.ui.View):
         row = self.cursor.fetchone()
         self.highest = row[0] if row else 0
 
-    
+    #Embed Updater
     async def update_embed(self, interaction: discord.Interaction, result: str):
         if self.count > self.highest:
             self.highest = self.count  
 
-        color = discord.Color.green() if result == "Correct" else color = discord.Color.red()
+        color = discord.Color.green() if result == "Correct" else discord.Color.red()   
 
         embed = discord.Embed(
             title= f"{result}\nNew Number: {self.random_number}",
-            description=(f"Current Streak: {self.count}\nHighest Score: {self.highest}"),
+            description=(f"Current Score: {self.count}\nHighest Score: {self.highest}"),
             color=color
         )
 
         await interaction.response.edit_message(embed=embed, view=self)
     
+    #Higher Button
     @discord.ui.button(label="Higher", style=discord.ButtonStyle.grey)
     async def button_higher(self, interaction: discord.Interaction, button: discord.ui.Button):
         print("Higher Button Clicked")
         new_number = random.randint(1, 100)
-        if new_number > self.random_number:
+        if new_number >= self.random_number:
             result = "Correct"
             self.count += 1
         else:
             result = "Incorrect"
-            if self.count >= self.highest:
+            if self.count > self.highest:
                 self.highest = self.count
             self.count = 0
         self.random_number = new_number
         await self.update_embed(interaction, result)
 
+    #Lower Button
     @discord.ui.button(label="Lower", style=discord.ButtonStyle.grey)
     async def button_lower(self, interaction: discord.Interaction, button: discord.ui.Button):
         print("Lower Button Clicked")
         new_number = random.randint(1, 100)
-        if new_number < self.random_number:
+        if new_number <= self.random_number:
             result = "Correct"
             self.count += 1
         else:
@@ -74,6 +77,7 @@ class Buttons(discord.ui.View):
         self.random_number = new_number
         await self.update_embed(interaction, result)
 
+    #Close Button
     @discord.ui.button(label="Close", style=discord.ButtonStyle.grey)
     async def button_end(self, interaction: discord.Interaction, button: discord.ui.Button):
         print("Close Button Clicked")
@@ -99,8 +103,11 @@ class highlowCog(commands.Cog):
 
     @commands.hybrid_command(aliases=["hl"])
     async def highlow(self, ctx):
-        view = Buttons()
-        embed = discord.Embed(title= f"Starting number: {view.random_number}", description="**HighLow**\nPredict whether the next number will be higher or lower", color=discord.Color.green())
+        view = Buttons(ctx.author.id, ctx.guild.id)
+        
+        highest_score = view.highest
+        
+        embed = discord.Embed(title= f"Starting number: {view.random_number}", description=f"**HighLow**\nPredict whether the next number will be higher or lower\nHighest Score: {highest_score}", color=discord.Color.green())
         await ctx.send(embed=embed, view=view)
 
     def cog_unload(self):
